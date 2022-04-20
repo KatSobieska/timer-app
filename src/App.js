b0e3424a7db52c065aeb9c82cd218df37bcfbf41
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-20 18:12:17 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Container from "./components/Container/Container.js";
+import Button from "./components/Button/Button.js";
+
+const App = () => {
+  return (
+    <Container>
+      <Button />
+      <Button />
+      <Button />
+    </Container>
+  );
+};
+
+export default App;
