9ccfb790301204579bc9c335e3765a8e7b5013d4
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-24 10:09:27 +0200

    Add timer functionality and and update buttons

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,18 +1,42 @@
 import styles from "./Timer.module.scss";
-import Buttons from "../Buttons/Buttons.js";
+import Button from "../Button/Button.js";
+import { useEffect, useState } from "react";
 
 const Timer = () => {
-  const start = () => console.log("hello");
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
 
-  const stop = () => console.log("hello");
+  let milliseconds = String(time).slice(-3).padStart(3, "0");
+  let seconds = String(Math.floor(time / 1000)).padStart(2, "0");
+  let minutes = String(Math.floor(seconds / 60)).padStart(2, "0");
+  let hours = String(Math.floor(minutes / 60)).padStart(2, "0");
 
-  const reset = () => console.log("hello");
+  useEffect(() => {
+    let timer;
+
+    if (start) {
+      timer = setInterval(() => {
+        setTime((prevValue) => prevValue + 1);
+      }, 1);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [start]);
 
   return (
     <div className={styles.timer}>
-      <h1>Hello1</h1>
-
-      <Buttons start={start} stop={stop} reset={reset} />
+      <h1>
+        {hours}:{minutes}:{seconds}.{milliseconds}
+      </h1>
+      <div className={styles.buttons}>
+        <Button onClick={() => setStart(true)}>Start</Button>
+        <Button onClick={() => setStart(false)}>Stop</Button>
+        <Button onClick={() => setTime(0)}>Reset</Button>
+      </div>
     </div>
   );
 };
